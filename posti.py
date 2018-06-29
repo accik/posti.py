6e20407ed4b42fd9358e5cc5f0c3182989d98075
varjolintu <sami.vanttinen@protonmail.com>
2018-06-29 08:14:52 +0300

    Add utf8 encoding

--- posti.py
+++ posti.py
@@ -8,23 +8,24 @@ import sys
 import time
 from datetime import datetime
 
-def getState(code):
+def get_state(code):
     req = requests.get("https://www.posti.fi/henkiloasiakkaat/seuranta/api/shipments/" + code)
     if req.status_code == 200:
         resp = json.loads(req.content)
         if len(resp['shipments']) > 0:
             event = resp['shipments'][0]['events'][0]
             desc = unicode(event['description']['fi']).encode('utf8')
+            location = unicode(event['locationName']).encode('utf8')
             timestamp = datetime.strptime(event['timestamp'], "%Y-%m-%dT%H:%M:%SZ")
-            newHour = timestamp.hour + time.timezone / -3600 + 1
-            timestamp = timestamp.replace(hour=newHour)
-            print "{0}: {1} {2}, {3}".format(code, desc, timestamp, event['locationName'])
+            new_hour = timestamp.hour + time.timezone / -3600 + 1
+            timestamp = timestamp.replace(hour=new_hour)
+            print "{0}: {1} {2}, {3}".format(code, desc, timestamp, location)
         else:
             print "Virheellinen seurantakoodi."
 
 def main(argv):
     for arg in argv:
-        getState(arg)
+        get_state(arg)
         
 if __name__ == "__main__":
     main(sys.argv[1:])
