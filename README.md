b27d480e2380934f15390eca555005b6dc99ba70
varjolintu <sami.vanttinen@protonmail.com>
2018-06-30 09:37:02 +0300

    Add command line parameter support and long event lists

--- README.md
+++ README.md
@@ -3,9 +3,32 @@ Hakee viimeisimmän tapahtuman Postin seurantakoodilla. Koodeja voi olla myös u
 
 Käyttö:
 ```
-./posti.py <seurantakoodi1> <seurantakoodi2> ... 
+usage: posti.py [-h] [-l] koodi [koodi ...]
 
+positional arguments:
+  koodi        seurantakoodi
+
+optional arguments:
+  -h, --help   show this help message and exit
+  -l, --lista  tulosta pitkä lista
+```
+
+```
+./posti.py <seurantakoodi1> <seurantakoodi2>
 <seurantakoodi1>: Lähetys on lajiteltu. 2018-06-28 07:54:39, OULU
 <seurantakoodi2>: Lähetys on vastaanotettu kuljetuksesta. 2018-06-28 12:52:34, Posti kuljetus
-...
+```
+
+```
+./posti.py <seurantakoodi1> <seurantakoodi2> -l
+<seurantakoodi1>: Lähetys on lajiteltu. 2018-06-30 06:22:28, OULU
+<seurantakoodi1>: Lähetys on vastaanotettu kuljetuksesta. 2018-06-30 05:48:18, Posti kuljetus
+<seurantakoodi1>: Lähetys on kuljetuksessa. 2018-06-29 20:32:15, TAMPERE
+<seurantakoodi1>: Lähetys on rekisteröity. 2018-06-29 18:19:41, TAMPERE
+<seurantakoodi1>: Lähetys on lajiteltu. 2018-06-29 18:18:25, Lajittelukeskus TAMPERE
+<seurantakoodi1>: Lähetys ei ole vielä saapunut Postille, odotathan 2018-06-29 09:34:13, HELSINKI
+
+<seurantakoodi2>: Lähetys ei ole vielä saapunut Postille, odotathan 2018-06-30 08:32:11, HELSINKI
+```
+
 ```
--- posti.py
+++ posti.py
@@ -2,30 +2,39 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2018 Sami Vänttinen <sami.vanttinen@protonmail.com>
 
-import json
-import requests
-import sys
-import time
+import argparse, json, requests, sys, time
 from datetime import datetime
 
-def get_state(code):
+def get_event(event, code):
+    desc = unicode(event['description']['fi']).encode('utf8')
+    location = unicode(event['locationName']).encode('utf8')
+    timestamp = datetime.strptime(event['timestamp'], "%Y-%m-%dT%H:%M:%SZ")
+    new_hour = timestamp.hour + time.timezone / -3600 + 1
+    timestamp = timestamp.replace(hour=new_hour)
+    print "{0}: {1} {2}, {3}".format(code, desc, timestamp, location)
+
+def get_state(code, multiple):
     req = requests.get("https://www.posti.fi/henkiloasiakkaat/seuranta/api/shipments/" + code)
     if req.status_code == 200:
         resp = json.loads(req.content)
         if len(resp['shipments']) > 0:
-            event = resp['shipments'][0]['events'][0]
-            desc = unicode(event['description']['fi']).encode('utf8')
-            location = unicode(event['locationName']).encode('utf8')
-            timestamp = datetime.strptime(event['timestamp'], "%Y-%m-%dT%H:%M:%SZ")
-            new_hour = timestamp.hour + time.timezone / -3600 + 1
-            timestamp = timestamp.replace(hour=new_hour)
-            print "{0}: {1} {2}, {3}".format(code, desc, timestamp, location)
+            for event in resp['shipments'][0]['events']:
+                get_event(event, code)
+                if multiple == False:
+                    break
         else:
             print "Virheellinen seurantakoodi."
 
 def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--lista", action="store_true", help="tulosta pitkä lista")
+    parser.add_argument("koodi", nargs="+", help="seurantakoodi")
+    args = parser.parse_args()
+
     for arg in argv:
-        get_state(arg)
+        get_state(arg, args.lista)
+        if args.lista:
+            print ""
         
 if __name__ == "__main__":
     main(sys.argv[1:])
